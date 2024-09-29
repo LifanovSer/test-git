084350288704be39d4ea58be71c357deeccfbe0a
sergey <lifannnnof@mail.ru>
2024-09-29 18:30:01 +0300

    запушил из main

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from "react";
+import ImgLogo from "./img/img";
 
-import './App.css'
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(100);
 
   return (
     <>
-     <div className='wrapper'>
-      <div className="child">123</div>
-      <div className="child">456</div>
-      <div className="child">789</div>
-      <div className="child"></div>
-      <div className="child"></div>
-      <div className="child">4444444</div>
-      <div className="child">890</div>
-      <div className="child">999999999999999999999999999999999</div>
-      <div className="child">34343434343434344</div>
-     </div>
+      <div className="wrapper">
+        <div className="child">123</div>
+        <div className="child">456</div>
+        <div className="child">789</div>
+        <div className="child"></div>
+        <div className="child"></div>
+        <div className="child">4444444</div>
+        <div className="child">890</div>
+        <div className="child">999999999999999999999999999999999</div>
+        <div className="child">34343434343434344</div>
+        <ImgLogo />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/img/img.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ImgLogo = () => {
+  return <div>it's ImgLogo</div>;
+};
+
+export default ImgLogo;
